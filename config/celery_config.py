94fb6e2ab9231be08976d3d5d91f48f92c1847a6
sakenzi <ermekulysaken8@gmail.com>
2025-08-29 14:11:59 +0500

    add celery

--- config/celery_config.py
+++ config/celery_config.py
@@ -1,1 +1,16 @@
 from celery import Celery
+
+
+app = Celery(
+    'skill_berter',
+    broker='redis://localhost:6379/0',
+    backend='redis://locahost:6379/1'
+)
+
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+)
